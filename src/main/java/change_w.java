51a81b985cc70b5a572fab89d2c84ddf7bbeb2a3
zeimo、 <2878934647@qq.com>
2023-11-07 22:05:43 +0800

    Default Changelist

--- src/main/java/change_w.java
+++ src/main/java/change_w.java
@@ -1,4 +1,4 @@
-import javax.servlet.annotation.WebServlet;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -6,21 +6,17 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
 
-@WebServlet("/change_w")
+@WebServlet("/changew")
 public class change_w extends HttpServlet {
     private static final long serialVersionUID = 1L;
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/plain");
-        PrintWriter out = response.getWriter();
-
         // 获取前端传递的数据
         String username = request.getParameter("username");
         String wname = request.getParameter("wname");
         String gender = request.getParameter("gender");
         String wphone = request.getParameter("wphone");
         String waddress = request.getParameter("waddress");
-
         // 数据库连接信息
         String url = "jdbc:mysql://localhost:3306/kaoqin?useSSL=false&serverTimezone=UTC";
         String dbUser = "root";
@@ -29,34 +25,66 @@ public class change_w extends HttpServlet {
         try {
             // 加载数据库驱动
             Class.forName("com.mysql.cj.jdbc.Driver");
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
             // 建立数据库连接
             Connection connection = DriverManager.getConnection(url, dbUser, dbPassword);
 
-            // 构建更新SQL语句
-            String updateQuery = "UPDATE worker SET wname=?, gender=?, wphone=?, waddress=? WHERE wno=?";
-            PreparedStatement preparedStatement = connection.prepareStatement(updateQuery);
-            preparedStatement.setString(1, wname);
-            preparedStatement.setString(2, gender);
-            preparedStatement.setString(3, wphone);
-            preparedStatement.setString(4, waddress);
-            preparedStatement.setString(5, username);
-
-            // 执行更新操作
-            int rowsUpdated = preparedStatement.executeUpdate();
-
-            if (rowsUpdated > 0) {
-                out.println("修改成功");
+            // 调用方法插入数据
+            if (userchange(connection, username,wname,gender,wphone,waddress)) {
+                sendSuccessResponse(response);
             } else {
-                out.println("修改失败");
+                // 如果插入失败，发送失败响应
+                sendFailureResponse(response);
             }
-
-            // 关闭数据库连接
-            preparedStatement.close();
-            connection.close();
-        } catch (ClassNotFoundException | SQLException e) {
+        } catch (SQLException e) {
+            // 捕获数据库操作异常并打印
             e.printStackTrace();
-            out.println("修改失败：" + e.getMessage());
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);  // 设置响应状态为服务器内部错误
+        }
+
+}
+    private boolean userchange(Connection connection, String username , String wname,String gender,String wphone, String waddress) throws SQLException {
+
+    String updateQuery = "UPDATE worker SET wname=?, gender=?, wphone=?, waddress=? WHERE wno=?";
+        PreparedStatement preparedStatement = connection.prepareStatement(updateQuery);
+
+        preparedStatement.setString(1, wname);
+        preparedStatement.setString(2, gender);
+        preparedStatement.setString(3, wphone);
+        preparedStatement.setString(4, waddress);
+        preparedStatement.setString(5, username);
+
+        // 执行更新操作
+        int rowsUpdated = preparedStatement.executeUpdate();
+
+        if (rowsUpdated > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    // 发送成功响应的方法
+    private void sendSuccessResponse(HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");  // 设置响应内容类型为JSON
+        try (PrintWriter out = response.getWriter()) {
+            out.print("{\"success\": true}");  // 返回成功的JSON响应
+            out.flush();
+        }
+    }
+
+    // 发送失败响应的方法
+    private void sendFailureResponse(HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");  // 设置响应内容类型为JSON
+        try (PrintWriter out = response.getWriter()) {
+            out.print("{\"success\": false}");  // 返回失败的JSON响应
+            out.flush();
         }
     }
 }
-
--- src/main/webapp/change_info.js
+++ src/main/webapp/change_info.js
@@ -12,27 +12,39 @@ updateDateTime();
 // 每秒更新一次日期和时间
 setInterval(updateDateTime, 1000);
 function change_w() {
-    const username = localStorage.getItem('username');
-    const wname = document.getElementById("wname").value;
-    const gender = document.querySelector('input[name="gender"]:checked').value;
-    const wphone = document.getElementById("wphone").value;
-    const waddress = document.getElementById("waddress").value;
+    var username = localStorage.getItem('username');
+    var wname = document.getElementById("wname").value;
+    var genderElement = document.querySelector('input[name="gender"]:checked');
+    var gender = genderElement ? genderElement.value : ''; // 使用.value获取选中的值
+    var wphone = document.getElementById("wphone").value;
+    var waddress = document.getElementById("waddress").value;
 
     // 发送数据到后端Servlet
-    const xhr7 = new XMLHttpRequest();
-    xhr7.open("POST", 'http://localhost:8080/ServletMaven/change_w', true);
-    xhr7.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    if (!wname || !gender || !wphone || !waddress) {
+        alert("请填写完整的申请信息");
+    } else {
+        var xhr7 = new XMLHttpRequest();
+        xhr7.open("POST", 'http://localhost:8080/ServletMaven/changew', true);
+        xhr7.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
-    xhr7.onreadystatechange = function () {
-        if (xhr7.readyState === 4 && xhr7.status === 200) {
-            const response = xhr7.responseText;
-            alert(response); // 根据需要显示成功或失败消息
-        }
-    };
+        xhr7.onreadystatechange = function () {
+            if (xhr7.readyState == 4 && xhr7.status == 200) {
+                var response = JSON.parse(xhr7.responseText);
+                if (response.success) {
+                    alert("修改成功！");
+                } else {
+                    alert("修改失败");
+                }
+            } else {
+                console.error('修改出错:', xhr7.status, xhr7.statusText);
+            }
+        };
 
-    // 构建要发送的数据
-    const data7 = `username=${username}&wname=${wname}&gender=${gender}&wphone=${wphone}&waddress=${waddress}`;
+        // 构建要发送的数据
+        var data7 = `username=${username}&wname=${wname}&gender=${gender}&wphone=${wphone}&waddress=${waddress}`;
 
-    // 发送数据
-    xhr7.send(data7);
+        // 发送数据
+        xhr7.send(data7);
+        //alert(gender);
+    }
 }
